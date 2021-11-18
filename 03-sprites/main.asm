35f9b260dff80370fc8984a2a1c309a03474f6be
Lukas Hein <lukashein@protonmail.com>
2021-11-19 00:12:18 +0100

    Add sprites

---
+++ 03-sprites/main.asm
@@ -0,0 +1,62 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
+  RTI
+.endproc
+
+.import reset_handler
+
+.export main
+.proc main
+  ; write a palette
+  LDX PPUSTATUS
+  LDX #$3f
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDA #$29
+  STA PPUDATA
+  LDA #$19
+  STA PPUDATA
+  LDA #$09
+  STA PPUDATA
+  LDA #$0f
+  STA PPUMASK
+
+  ; write sprite data
+  LDA #$70
+  STA $0200 ; Y-coord of first sprite
+  LDA #$05
+  STA $0201 ; tile number of first sprite
+  LDA #$0f
+  STA $0202 ; attributes of first sprite
+  LDA #$80
+  STA $0203 ; X-coord of first sprite
+
+vblankwait:       ; wait for another vblank before continuing
+  BIT PPUSTATUS
+  BPL vblankwait
+
+  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
+  STA PPUCTRL
+  LDA #%00011110  ; turn on screen
+  STA PPUMASK
+forever:
+  JMP forever
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
+
+.segment "CHR"
+.incbin "graphics.chr"
